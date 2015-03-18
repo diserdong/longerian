57fe20ae14a6e563d9145697d64b9e2b0fe4336c
灰风 <huifeng.hxl@alibaba-inc.com>
2015-03-18 18:04:29 +0800

    调测布局，初步完成

---
+++ ABCAndroid/src/main/java/me/longerian/abcandroid/homepage/TMHomepageTest.java
@@ -0,0 +1,18 @@
+package me.longerian.abcandroid.homepage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import me.longerian.abcandroid.R;
+
+/**
+ * Created by huifeng.hxl on 2015/3/16.
+ */
+public class TMHomepageTest extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage_test);
+    }
+}
