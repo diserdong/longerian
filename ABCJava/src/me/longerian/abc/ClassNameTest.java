4bb23e082aed881072ce05559739096a8ec0b930
longerian <xlhongultimate@gmail.com>
2015-04-07 16:06:45 +0800

    测试

---
+++ ABCJava/src/me/longerian/abc/ClassNameTest.java
@@ -0,0 +1,13 @@
+package me.longerian.abc;
+
+/**
+ * Created by longerian on 15/4/7.
+ */
+public class ClassNameTest {
+
+    public static void main(String[] args) {
+        ClassNameTest test = new ClassNameTest();
+        System.out.println(test.getClass().getSimpleName());
+    }
+
+}
